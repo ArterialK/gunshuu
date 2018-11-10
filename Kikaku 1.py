aa4a40a0e6f3f2437f1fab10f985f6180153f00e
ArterialK <42286659+ArterialK@users.noreply.github.com>
2018-11-10 14:25:04 -0600

    Update Kikaku 1.py

--- Kikaku 1.py
+++ Kikaku 1.py
@@ -1,1 +1,7 @@
+############ Proyecto 1 del prro de EDAII    ################################
+############ si no recuerdan que es es hacer un codigo que ##################
+############ Ordene una lista en forma de triangulo o sierra ################
+############ punto extra si solo es usando una funcion ######################
+############ es decir que se use la misma funcion para ambos ################
+
 
